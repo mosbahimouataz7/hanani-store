e4b1b47de4af6e99ece390718320480c7c48a3b3
mosbahimouataz7 <mosbahimouataz7@gmail.com>
2026-02-08 02:48:32 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,16 @@
-const cacheName = 'hanani-v2';
+// اسم النسخة - تم تحديثه لضمان تحميل التعديلات الجديدة
+const cacheName = 'hanani-store-v2';
+
+// الملفات التي سيتم حفظها
+// استخدام ./ يضمن العمل داخل المجلد الفرعي github.io/hanani-store/
 const assets = [
-  '/hanani-store/',
-  '/hanani-store/index.html',
+  './',
+  './index.html',
+  './manifest.json',
   'https://fonts.googleapis.com/css2?family=Tajawal:wght@400;700&display=swap'
 ];
 
+// تثبيت ملف الخدمة
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(cacheName).then(cache => {
@@ -13,6 +19,18 @@ self.addEventListener('install', e => {
   );
 });
 
+// تفعيل الخدمة وحذف الكاش القديم
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.map(key => {
+        if(key !== cacheName) return caches.delete(key);
+      }));
+    })
+  );
+});
+
+// جلب البيانات (Offline Strategy)
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(res => {
