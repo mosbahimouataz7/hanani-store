42344b51a8db989b3056d247d9b61bc1ce4322db
mosbahimouataz7 <mosbahimouataz7@gmail.com>
2026-02-08 02:02:24 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+{
+  "name": "حناني بيت العطور والبخور",
+  "short_name": "Hanani Store",
+  "start_url": "/hanani-store/",
+  "scope": "/hanani-store/",
+  "display": "standalone",
+  "background_color": "#f0f2f5",
+  "theme_color": "#1b4332",
+  "orientation": "portrait",
+  "icons": [
+    {
+      "src": "https://cdn-icons-png.flaticon.com/512/2830/2830305.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "https://cdn-icons-png.flaticon.com/512/2830/2830305.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
